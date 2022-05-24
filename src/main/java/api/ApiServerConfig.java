c4f34edaefda54be066de9d937dfcc5215ac8426
jisang Park <groundp@adenasoft.com>
2022-05-24 15:32:59 +0900

    initial commit

---
+++ src/main/java/api/ApiServerConfig.java
@@ -0,0 +1,49 @@
+package api;
+import java.net.InetSocketAddress;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@ImportResource("classpath:spring/hsqlApplicationContext.xml")
+@ComponentScan("api.core, api, api.service")
+@PropertySource("classpath:api-server.properties")
+public class ApiServerConfig {
+    @Value("${boss.thread.count}")
+    private int bossThreadCount;
+
+    @Value("${worker.thread.count}")
+    private int workerThreadCount;
+
+    @Value("${tcp.port}")
+    private int tcpPort;
+
+    @Bean(name = "bossThreadCount")
+    public int getBossThreadCount() {
+        return bossThreadCount;
+    }
+
+    @Bean(name = "workerThreadCount")
+    public int getWorkerThreadCount() {
+        return workerThreadCount;
+    }
+
+    public int getTcpPort() {
+        return tcpPort;
+    }
+
+    @Bean(name = "tcpSocketAddress")
+    public InetSocketAddress tcpPort() {
+        return new InetSocketAddress(tcpPort);
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
